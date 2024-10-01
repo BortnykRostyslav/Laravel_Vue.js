f9b6521523424210126d91b0222faf33063bab67
Bortnyk Rostyslav <bortnikrostislav370@gmail.com>
2024-10-01 19:49:21 +0300

    Work with "vue"

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,10 +1,64 @@
 <script setup>
 
+import { reactive, computed } from 'vue';
+
+import SinglePostComponent from "@/components/SinglePostComponent.vue";
+
+const persons = reactive([
+    {
+        id: 1,
+        name: 'Ivan',
+        age: 26
+    },
+    {
+        id: 2,
+        name: 'Olena',
+        age: 30
+    },
+    {
+        id: 3,
+        name: 'Mykola',
+        age: 28
+    },
+    {
+        id: 4,
+        name: 'Anna',
+        age: 22
+    }
+]);
+
+function sayHello(){
+    console.log('Hello');
+}
+
+function IvanJob(){
+    return persons.name + ' працює програмістом.'
+}
+
+const oldPersons = computed(() => {
+    return persons.filter(person => person.age > 25);
+});
+
 </script>
 
 <template>
     <div>
-        11111111111111
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in oldPersons">
+                <th scope="row">{{ person.id}}</th>
+                <td>{{ person.name}}</td>
+                <td>{{ person.age}}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
