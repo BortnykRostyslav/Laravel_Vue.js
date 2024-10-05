de6415e80b31eacdb150a817cfb40e22fe56fa81
Bortnyk Rostyslav <bortnikrostislav370@gmail.com>
2024-10-05 23:29:16 +0300

    CreateComponent.vue

---
+++ resources/js/components/CreateComponent.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+import {ref} from 'vue';
+import axios from 'axios';
+
+import data from "bootstrap/js/src/dom/data.js";
+
+
+const name = ref(null);
+const age = ref(null);
+const job = ref(null);
+
+const addData = () => {
+    // @ts-ignore
+    axios.post('api/people', {
+        name: name.value,
+        age: age.value,
+        job: job.value
+    })
+        .then(response => {
+            name.value = null;
+            age.value = null;
+            job.value = null;
+
+            console.log('Data added successfully:', response.data);
+        })
+        .catch(error => {
+            console.error('Error adding data:', error);
+        });
+};
+
+</script>
+
+<template>
+    <div class="w-25">
+        <div class="mb-3">
+            <input type="text" class="form-control" id="name" v-model="name" placeholder="name">
+        </div>
+        <div class="mb-3">
+            <input type="number" class="form-control" id="age" v-model="age" placeholder="age">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" id="job" v-model="job" placeholder="job">
+        </div>
+        <div class="mb-3">
+            <input @click.prevent="addData" type="submit" class="btn btn-primary" value="Add">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -3,44 +3,45 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-import SinglePostComponent from "@/components/SinglePostComponent.vue";
+import CreateComponent from "@/components/CreateComponent.vue";
 import data from "bootstrap/js/src/dom/data.js";
 
 const persons = ref(null);
 
-function getPersons() {
-    axios.get('/persons')
-        .then(response  => {
-            persons.value = response.data;
-        })
-}
+// function getPersons() {
+//     axios.get('/persons')
+//         .then(response  => {
+//             persons.value = response.data;
+//         })
+// }
 
-onMounted(() => {
-    getPersons();
-});
+// onMounted(() => {
+//     getPersons();
+// });
 
 </script>
 
 <template>
     <div>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Job</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="person in persons">
-                <th scope="row">{{ person.id }}</th>
-                <td>{{ person.name }}</td>
-                <td>{{ person.age }}</td>
-                <td>{{ person.job }}</td>
-            </tr>
-            </tbody>
-        </table>
+        <CreateComponent></CreateComponent>
+<!--        <table class="table">-->
+<!--            <thead>-->
+<!--            <tr>-->
+<!--                <th scope="col">ID</th>-->
+<!--                <th scope="col">Name</th>-->
+<!--                <th scope="col">Age</th>-->
+<!--                <th scope="col">Job</th>-->
+<!--            </tr>-->
+<!--            </thead>-->
+<!--            <tbody>-->
+<!--            <tr v-for="person in persons">-->
+<!--                <th scope="row">{{ person.id }}</th>-->
+<!--                <td>{{ person.name }}</td>-->
+<!--                <td>{{ person.age }}</td>-->
+<!--                <td>{{ person.job }}</td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
     </div>
 </template>
 
