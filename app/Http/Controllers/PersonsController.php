f4399a706f5bd8dfa62c7bed8c4d9bade230db35
Bortnyk Rostyslav <bortnikrostislav370@gmail.com>
2024-10-05 23:29:00 +0300

    Create controller

--- app/Http/Controllers/PersonsController.php
+++ app/Http/Controllers/PersonsController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
-
-class PersonsController extends BaseController
+class PersonsController extends Controller
 {
     public function __invoke()
     {
